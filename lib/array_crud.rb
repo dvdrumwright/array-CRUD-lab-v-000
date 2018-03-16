2b8516903c4677a819b49d23b28aba1422078a75
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-03-16 14:37:32 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,7 +43,7 @@ end
 
 
 def retrieve_last_element_from_array(array)
-    retrieve_last_element_from_array["red", "blue", "arrays!"]
-   
+    retrieve_last_element_from_array = ["red", "blue", "arrays!"]
+   retrieve_last_element_from_array[2]
     
 end
